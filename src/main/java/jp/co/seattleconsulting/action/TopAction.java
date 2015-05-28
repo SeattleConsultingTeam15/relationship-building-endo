58caab3f444e25e8220d018d9a28aa4bf343c964
Takayuki Endo <takayuki.endo15@gmail.com>
2015-05-28 16:35:37 +0900

    JSON形式でやってみた

--- src/main/java/jp/co/seattleconsulting/action/TopAction.java
+++ src/main/java/jp/co/seattleconsulting/action/TopAction.java
@@ -3,12 +3,18 @@
  */
 package jp.co.seattleconsulting.action;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletResponse;
 
 import jp.co.seattleconsulting.dto.LoginDto;
 import jp.co.seattleconsulting.service.TopService;
+import net.arnx.jsonic.JSON;
 
 import org.seasar.struts.annotation.Execute;
+import org.seasar.struts.util.ResponseUtil;
 
 /**
  * TOP画面で行われる処理の制御を行うクラス.
@@ -34,12 +40,26 @@ public class TopAction {
 	 * @return TOP画面のパス
 	 */
 	@Execute(validator = false)
-	public String index() {
+	public String index() throws IOException {
+		HttpServletResponse httpServletResponse = ResponseUtil.getResponse();
+		httpServletResponse.setContentType("application/json");
+		PrintWriter sendPoint = new PrintWriter(httpServletResponse.getOutputStream());
+
+		sendPoint.println(JSON.encode(topService.getCountOfActive()));
+		sendPoint.println(JSON.encode(topService.getCountOfTalked()));
+		sendPoint.println(JSON.encode(topService.getCountOfNotTalked()));
+		sendPoint.println(JSON.encode(topService.getParcentageOfTalked()));
+		sendPoint.println(JSON.encode(100 - topService.getParcentageOfTalked()));
+		sendPoint.flush();
+		sendPoint.close();
+
+		/**
 		countOfActive = topService.getCountOfActive();
 		countOfTalked = topService.getCountOfTalked();
 		countOfNotTalked = topService.getCountOfNotTalked();
 		parcentageOfTalked = topService.getParcentageOfTalked();
 		parcentageOfNotTalked = 100 - topService.getParcentageOfTalked();
+		**/
 
 		return "top.jsp";
 	}
